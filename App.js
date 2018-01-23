ff02a1ddf5bb35b206a33e895c31650576547c47
simonh <chocy23@hotmail.com>
2018-01-23 12:31:55 +1100

    first

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import Store from './src/Store';
+import { Header, CryptoContainer } from './src/components';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={Store}>
+      <View>
+        <Header />
+        <CryptoContainer />
+      </View>
+      </Provider>
+    );
+  }
+}
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Header from './header.js'
+import CryptoContainer from './CryptoContainer';
+
+export { Header, CryptoContainer };
